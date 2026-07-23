2bb109461fb83af12e0d4aecf7917560892fa451
Adrian <adrianbercovici.work@gmail.com>
2026-07-23 16:30:23 +0300

    Move CbcHmacEnvelope to Cryptography

--- Cripty.Cryptography/Models/CbcHmacEnvelope.cs
+++ Cripty.Cryptography/Models/CbcHmacEnvelope.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Cripty.Storage.Formats
+namespace Cripty.Cryptography.Models
 {
     public sealed class CbcHmacEnvelope
     {
         public required byte[] Iv { get; init; }
         public required byte[] Ciphertext { get; init; }
-        public required byte[] Hmac { get; init; }
+        public required byte[] Mac { get; init; }
     }
 }
--- Cripty.Storage/Formats/ManifestEnvelope.cs
+++ Cripty.Storage/Formats/ManifestEnvelope.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Cripty.Cryptography.Models;
 
 namespace Cripty.Storage.Formats
 {

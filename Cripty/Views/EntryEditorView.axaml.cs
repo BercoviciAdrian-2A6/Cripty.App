81e5098efd28c098fde60bcb6ceeebae734683bf
Adrian <adrianbercovici.work@gmail.com>
2026-08-09 00:13:14 +0300

    Ui work 3.11

--- Cripty/Views/EntryEditorView.axaml.cs
+++ Cripty/Views/EntryEditorView.axaml.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Diagnostics;
 using Avalonia.Controls;
+using Avalonia.Input.Platform;
+using Avalonia.Interactivity;
+using Cripty.ViewModels;
 
 namespace Cripty.Views;
 
@@ -7,5 +12,32 @@ public partial class EntryEditorView : UserControl
     public EntryEditorView()
     {
         InitializeComponent();
+    }
+
+    private async void CopyFieldContent(
+        object? sender,
+        RoutedEventArgs eventArgs)
+    {
+        if (sender is not Button
+            {
+                DataContext:
+                    EntryTextFieldViewModel field
+            } ||
+            TopLevel.GetTopLevel(this)?.Clipboard
+                is not { } clipboard)
+        {
+            return;
+        }
+
+        try
+        {
+            await clipboard.SetTextAsync(
+                field.Text);
+        }
+        catch (Exception exception)
+        {
+            Debug.WriteLine(
+                $"Could not copy field content: {exception}");
+        }
     }
 }
